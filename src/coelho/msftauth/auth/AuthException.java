0132a7ebc348cdf260d81388c0b95aebbdfff33e
liulihaocai <liulihaocaiqwq@gmail.com>
2022-01-30 21:58:26 +0800

    better framework & remove javafx auther

--- src/coelho/msftauth/auth/AuthException.java
+++
@@ -1,9 +0,0 @@
-package coelho.msftauth.auth;
-
-public class AuthException extends Exception {
-
-	public AuthException(Exception exception) {
-		super(exception);
-	}
-
-}
---
+++ src/test/java/coelho/msftauth/MsftAuth.java
@@ -0,0 +1,9 @@
+package coelho.msftauth;
+
+public class MsftAuth {
+
+	private static final String MS_ACCESSTOKEN = "";
+
+	public static void main(String[] args) {
+	}
+}
